6fbf8854cf0eba97e6efe8193471de2766faa2c9
mithunrana <github@12M>
2020-11-03 01:00:13 +0600

    product category added

--- resources/views/UI/inc/categorysidebar.blade.php
+++ resources/views/UI/inc/categorysidebar.blade.php
@@ -1,18 +1,3 @@
-
-<div class="pc-title">
-    <h4 style="color:#d71920;">Brand</h4>
-</div>
-<div class="accordion" id="accordionExample2">
-    @foreach(\App\ProductsBrand::all() as $Brand)
-        <div class="cat-box">
-            <div class="cat-item" id="h1">
-                <a href="{{asset('')}}brand/{{$Brand->BrandUrl}}" style="border: 0;" >{{$Brand->BrandName}}</a>
-            </div>
-        </div>
-    @endforeach
-</div>
-
-
 <div class="pc-title">
     <h4 style="color:#d71920;">Categories</h4>
 </div>
@@ -20,7 +5,7 @@
     @foreach($Categories as $Category)
         <div class="cat-box">
             <div class="cat-item" id="h1">
-                <a style="border: 0;" type="button" data-toggle="collapse" data-target="#THI{{$Category->id}}">{{$Category->CategoryName}}<i class="fa fa-plus"></i></a>
+                <a style="padding: 16px 6px;border: 0; border-bottom: 1px solid red;" type="button" data-toggle="collapse" data-target="#THI{{$Category->id}}">{{$Category->CategoryName}}<i style="margin-top:3px" class="fa fa-plus"></i></a>
             </div>
             <div id="THI{{$Category->id}}" class="collapse cat-body" aria-labelledby="h1" data-parent="#accordionExample2">
                 <div class="card-body">
